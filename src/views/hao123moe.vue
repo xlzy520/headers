e4205d8cc604581eedc994b81ebd00379ad58dff
zhibi <1017866168@qq.com>
2018-07-18 14:13:34 +0800

    增加开始页和hao123萌主页的header

---
+++ src/views/hao123moe.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'hao123moe'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-container">
+    <div class="headers-title">
+
+    </div>
+    <div class="headers-content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
