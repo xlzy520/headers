ebe7f0177aeebb1396fc938b534dca91e75453e2
zhibi <1017866168@qq.com>
2018-07-18 18:48:13 +0800

    完善

--- src/views/hao123moe.vue
+++ src/views/hao123moe.vue
@@ -1,5 +1,19 @@
 <template>
-
+  <div class="header-placeholder">
+    <div class="header">
+      <div class="header-container">
+        <span class="logo-section">
+          <a href="#" class="hao123-logo">hao123</a>
+          <a href="#" class="sup-logo"></a>
+          <a href="#" class="sub-logo"></a>
+          <a href="#" class="sub-logo"></a>
+          <a href="#" class="sub-logo"></a>
+        </span>
+        <div class="tool-nav userinfo"></div>
+        <div class="tool-nav"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +22,34 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less">
+  @initHeight: 731;
+  .height(@height){
+    height: (@height/731)*100vh;
+  }
+.header-placeholder{
+  width: 100%;
+  .height(51);
 
+  .header{
+    position: fixed;
+    width: 100%;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    border-bottom: 1px solid #eee;
+    background-color: #fff;
+    background-color: rgba(255,255,255,.9);
+
+    .header-container{
+
+      .logo-section{
+        .height(50);
+        .hao123-logo{
+
+        }
+      }
+    }
+  }
+}
 </style>
