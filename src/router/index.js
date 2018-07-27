d554b0ac45da34afbf502ada51e6244beb901774
zhibi <1017866168@qq.com>
2018-07-27 15:04:41 +0800

    复制完整的title

--- src/router/index.js
+++ src/router/index.js
@@ -12,12 +12,12 @@ export default new Router({
     },
     {
       path: '/hao123moe',
-      name: '好123萌主页',
+      name: 'hao123萌主页 - 只属于你的二次元主页_hao123上网导航',
       component: () => import('@/views/hao123moe')
     },
     {
       path: '/bilibili',
-      name: '哔哩哔哩',
+      name: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili',
       component: () => import('@/views/bilibili')
     }
   ]
